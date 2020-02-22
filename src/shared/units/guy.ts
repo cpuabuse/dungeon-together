ae90572d5aac90f7dfd34ec38d7fadf9126cd148
Yano <51319482+yanocv@users.noreply.github.com>
2020-02-22 16:06:28 +0900

    Add: Grid outline #2

---
+++ src/shared/units/guy.ts
@@ -0,0 +1,15 @@
+/*
+	File: src/shared/units/guys.ts
+	cpuabuse.com
+*/
+
+/**
+ * Avatar/ player.
+ */
+
+import { Unit } from "../world/unit";
+
+/**
+ * Avatar for the user or player.
+ */
+export class Guy extends Unit {}
