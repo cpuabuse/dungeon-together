5c1f2ecd8c4bab16ff4c573754f491c3f029dbf2
Ali <74129604+alinumbercrunch@users.noreply.github.com>
2020-12-19 18:14:43 +0900

    Add: Trap folder

---
+++ src/app/server/entities/exclusive/traps/trap.ts
@@ -0,0 +1,15 @@
+/*
+	Copyright 2020 cpuabuse.com
+	Licensed under the ISC License (https://opensource.org/licenses/ISC)
+*/
+
+/**
+ * Trap.
+ */
+
+import { Exclusive } from "../../exclusive";
+
+/**
+ * Actual trap. The player can fail or loose HP depending on the trap class (spike/hole)
+ */
+export class Trap extends Exclusive {}
