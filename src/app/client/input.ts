46c6750cd8e66f1d8b4ff55db6038030e1a43e0d
Filipe Oliveira <52853539+oliverjiu@users.noreply.github.com>
2021-02-23 17:56:37 +0900

    Add: Input file #32

---
+++ src/app/client/input.ts
@@ -0,0 +1,13 @@
+/*
+	Copyright 2021 cpuabuse.com
+	Licensed under the ISC License (https://opensource.org/licenses/ISC)
+*/
+
+/**
+ * @file File for handling user input.
+ */
+
+/**
+ * A class representing a sequencies of users input, such as keyboard, mouse, touch.
+ */
+export class Input {}
