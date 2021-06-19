c8aae823df2f784990abff6582673225627e61e6
Yano <51319482+yanocv@users.noreply.github.com>
2021-06-19 18:07:49 +0900

    Add: Debug file

---
+++ src/app/vue/debug.vue
@@ -0,0 +1,20 @@
+<!--Component to display the debug menu-->
+
+<template>
+	<div>
+		<p>cpuabuse</p>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	/**
+	 *
+	 */
+	setup() {}
+});
+</script>
+
+<style scoped></style>
