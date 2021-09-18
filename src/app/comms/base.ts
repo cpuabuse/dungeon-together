81ad5e78eef1fa155703bc7076bf9add3d041bf2
Herman <35882542+winterhotlatte@users.noreply.github.com>
2021-09-18 11:41:54 +0900

    Add: Explanation for non recursive core base

--- src/app/comms/base.ts
+++ src/app/comms/base.ts
@@ -11,6 +11,9 @@ import { CoreUniverse } from "./universe";
 
 /**
  * Non-recursive minimal outline for base instance.
+ *
+ * Client/server entity/cell/grid/shard classes, extend core base, which contains core universe. Core universe contains core entity/cell/grid/shard classes, which also extend core base. This creates a primary recursion.
+ * When the client/server entity/cell/grid/shard extends the core base factory produced class, within generic constrains of that factory (e.g. `C extends CoreBaseClassNonRecursive`), if the universe in this interface were to be a core universe, it would create an additional recursive loop, which would not be resolved by the compiler. Thus, the type constraints should be using this, more vague interface, where universe is `any`, so that only recursion that happens, is the primary recursion.
  */
 export interface CoreBaseNonRecursive {
 	/**
