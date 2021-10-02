e4cd7f923d2f2b8046eca90316078e4f1d7b9576
Your Name <a.haddi@web.de>
2021-10-02 12:33:07 +0900

    Add: Get method for shard, grid and cell UUID

--- src/app/common/uuid-search.ts
+++ src/app/common/uuid-search.ts
@@ -63,6 +63,30 @@ export function CoreUniverseUuidSearchFactory<C extends CoreUniverseClassAbstrac
 		 *
 		 * @returns The cell, or default cell if not found
 		 */
+		public getCellByUuid({
+			cellUuid
+		}: {
+			/**
+			 * Cell UUID.
+			 */
+			cellUuid: Uuid;
+		}): this["cellsIndex"] extends Map<Uuid, infer T> ? T : never {
+			// Casting since somehow inference throws error with polymorphic this
+			let cell: (this["cellsIndex"] extends Map<Uuid, infer T> ? T : never) | undefined = this.cellsIndex.get(
+				cellUuid
+			) as (this["cellsIndex"] extends Map<Uuid, infer T> ? T : never) | undefined;
+			if (cell === undefined) {
+				// Casting since somehow inference throws error with polymorphic this
+				return this.getCell(this.defaultEntityPath) as this["cellsIndex"] extends Map<Uuid, infer T> ? T : never;
+			}
+			return cell;
+		}
+
+		/**
+		 * Gets a entity by UUID.
+		 *
+		 * @returns The cell, or default cell if not found
+		 */
 		public getEntityByUuid({
 			entityUuid
 		}: {
@@ -80,6 +104,54 @@ export function CoreUniverseUuidSearchFactory<C extends CoreUniverseClassAbstrac
 				return this.getEntity(this.defaultEntityPath) as this["entitiesIndex"] extends Map<Uuid, infer T> ? T : never;
 			}
 			return entity;
+		}
+
+		/**
+		 * Gets a grid by UUID.
+		 *
+		 * @returns The grid, or default grid if not found
+		 */
+		public getGridByUuid({
+			gridUuid
+		}: {
+			/**
+			 * Grid UUID.
+			 */
+			gridUuid: Uuid;
+		}): this["gridsIndex"] extends Map<Uuid, infer T> ? T : never {
+			// Casting since somehow inference throws error with polymorphic this
+			let grid: (this["gridsIndex"] extends Map<Uuid, infer T> ? T : never) | undefined = this.gridsIndex.get(
+				gridUuid
+			) as (this["gridsIndex"] extends Map<Uuid, infer T> ? T : never) | undefined;
+			if (grid === undefined) {
+				// Casting since somehow inference throws error with polymorphic this
+				return this.getGrid(this.defaultEntityPath) as this["gridsIndex"] extends Map<Uuid, infer T> ? T : never;
+			}
+			return grid;
+		}
+
+		/**
+		 * Gets a shard by UUID.
+		 *
+		 * @returns The shard, or default shard if not found
+		 */
+		public getShardByUuid({
+			shardUuid
+		}: {
+			/**
+			 * Shard UUID.
+			 */
+			shardUuid: Uuid;
+		}): this["shardsIndex"] extends Map<Uuid, infer T> ? T : never {
+			// Casting since somehow inference throws error with polymorphic this
+			let shard: (this["shardsIndex"] extends Map<Uuid, infer T> ? T : never) | undefined = this.shardsIndex.get(
+				shardUuid
+			) as (this["shardsIndex"] extends Map<Uuid, infer T> ? T : never) | undefined;
+			if (shard === undefined) {
+				// Casting since somehow inference throws error with polymorphic this
+				return this.getShard(this.defaultEntityPath) as this["shardsIndex"] extends Map<Uuid, infer T> ? T : never;
+			}
+			return shard;
 		}
 	}
 
