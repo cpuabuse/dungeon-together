09928e74536dc875aa281e8063cce8884db78f99
Ali <a.haddi@web.de>
2022-12-11 18:45:40 +0900

    Add: Progress bar class

--- src/app/client/progess-bar.ts
+++ src/app/client/progess-bar.ts
@@ -8,9 +8,80 @@
  */
 
 import Color from "color";
-import { Geometry, Shader } from "pixi.js";
+import { Geometry, Program, Shader } from "pixi.js";
 import { Palette } from "../common/color";
 import { ClientShard } from "./shard";
+
+/**
+ *  VertexSrc.
+ */
+const vertexSrc: string = `
+precision mediump float;
+attribute vec2 coord;
+uniform mat3 translationMatrix;
+uniform mat3 projectionMatrix;
+uniform float width;
+varying float relativeWidth;
+varying float relativeHeight;
+
+void main() {
+	relativeWidth = coord.x / width;
+	relativeHeight = coord.y / width;
+	gl_Position = vec4((projectionMatrix * translationMatrix * vec3(coord, 1.0)).xyz, 1.0);
+}
+`;
+
+/**
+ *  FragmentSrc.
+ */
+const fragmentSrc: string = `
+precision mediump float;
+varying float relativeWidth;
+varying float relativeHeight;
+uniform vec3 secondaryColor;
+uniform vec3 mainColor;
+uniform vec3 borderColor;
+uniform float secondaryColorIntensity;
+uniform float maxValue;
+uniform float value;
+uniform float borderRatio;
+uniform float height;
+
+void main() {
+	float ratio = value / maxValue;
+
+	if (relativeWidth >= borderRatio && (relativeWidth <= 1.0 - borderRatio) && relativeHeight >= borderRatio && (relativeHeight <= height - borderRatio)) {
+		if (relativeWidth <= ratio) {
+			gl_FragColor = vec4(mainColor + (secondaryColor - mainColor) * exp((relativeWidth / ratio - 1.0) * secondaryColorIntensity) * relativeWidth / ratio, 1.0);
+		} else {
+			gl_FragColor = vec4(secondaryColor, 1.0);
+		}
+	} else {
+		gl_FragColor = vec4(borderColor, 1.0);
+	}
+}
+`;
+
+// Add dynamic size change
+/**
+ * Width.
+ */
+let width: number = 1.0;
+
+/**
+ * Max value.
+ */
+let maxValue: number = 37;
+
+/**
+ * Value.
+ */
+let value: number = 5;
+
+/**
+ * Height.
+ */
+const height: number = 0.15;
 
 /**
  * HP bar parts tuple.
@@ -67,63 +138,35 @@ export class ProgressBar {
 	public colors: HpBarColors;
 
 	/**
+	 *Shader.
+	 */
+	public shader: Shader;
+
+	/**
 	 *  Geometry for the progress bar.
 	 */
-	private static geometry: Geometry;
+	private static geometry: Geometry = ((): Geometry => {
+		const geometry: Geometry = new Geometry().addAttribute(
+			"coord",
+			[0, 0, 0, height, 1, height, 1, 0].map(element => element * width),
+			2
+		);
+		geometry.addIndex([0, 1, 2, 0, 2, 3]);
+		return geometry;
+	})();
+
+	/**
+	 *  Program.
+	 *
+	 * @param vertexSrc - The source of the vertex shader
+	 * @param fragmentSrc - The source of the fragment shader
+	 */
+	private static program: Program = new Program(vertexSrc, fragmentSrc, `ProgressBarProgram`);
 
 	/**
 	 * Mesh.
 	 */
 	// private mesh: Mesh<Shader>;
-
-	/**
-	 * Shader.
-	 */
-	private static shader: Shader = Shader.from(
-		`
-precision mediump float;
-attribute vec2 coord;
-uniform mat3 translationMatrix;
-uniform mat3 projectionMatrix;
-uniform float width;
-varying float relativeWidth;
-varying float relativeHeight;
-
-void main() {
-	relativeWidth = coord.x / width;
-	relativeHeight = coord.y / width;
-	gl_Position = vec4((projectionMatrix * translationMatrix * vec3(coord, 1.0)).xyz, 1.0);
-}
-`,
-		`
-precision mediump float;
-varying float relativeWidth;
-varying float relativeHeight;
-uniform vec3 secondaryColor;
-uniform vec3 mainColor;
-uniform vec3 borderColor;
-uniform float secondaryColorIntensity;
-uniform float maxValue;
-uniform float value;
-uniform float borderRatio;
-uniform float height;
-
-void main() {
-	float ratio = value / maxValue;
-
-	if (relativeWidth >= borderRatio && (relativeWidth <= 1.0 - borderRatio) && relativeHeight >= borderRatio && (relativeHeight <= height - borderRatio)) {
-		if (relativeWidth <= ratio) {
-			gl_FragColor = vec4(mainColor + (secondaryColor - mainColor) * exp((relativeWidth / ratio - 1.0) * secondaryColorIntensity) * relativeWidth / ratio, 1.0);
-		} else {
-			gl_FragColor = vec4(secondaryColor, 1.0);
-		}
-	} else {
-		gl_FragColor = vec4(borderColor, 1.0);
-	}
-}
-`,
-		{}
-	);
 
 	/**
 	 * Client shard.
@@ -151,5 +194,35 @@ void main() {
 	}) {
 		this.shard = shard;
 		this.colors = colors ?? neutralHpBarColors;
+
+		// Add color on main bar
+		let mainColor: Array<number> = this.colors.foregroundMain
+			.rgb()
+			.array()
+			.map(element => element / 255);
+
+		// Add color on secondary bar
+		let secondaryColor: Array<number> = this.colors.background
+			.rgb()
+			.array()
+			.map(element => element / 255);
+
+		// Add color on border
+		let borderColor: Array<number> = this.colors.border
+			.rgb()
+			.array()
+			.map(element => element / 255);
+
+		this.shader = new Shader(ProgressBar.program, {
+			borderColor,
+			borderRatio: 0.01,
+			height,
+			mainColor,
+			maxValue,
+			secondaryColor,
+			secondaryColorIntensity: 10,
+			value,
+			width
+		});
 	}
 }
