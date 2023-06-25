332cd96126ee59903e9575d060985a01f508fc8e
Ali <a.haddi@web.de>
2023-06-25 14:41:01 +0900

    Add: Slider component

--- src/app/vue/components/overlay-list-item-slider.vue
+++ src/app/vue/components/overlay-list-item-slider.vue
@@ -1,11 +1,36 @@
 <!-- Slider element -->
 
 <template>
-	<div>Slider</div>
+	<VSlider v-model="value" min="0" max="100" :step="1">
+		<template v-slot:prepend>
+			<VBtn size="small" variant="text" icon="fa-minus" @click="decrement"></VBtn>
+		</template>
+
+		<template v-slot:append>
+			<VBtn size="small" variant="text" icon="fa-plus" @click="increment"></VBtn>
+		</template>
+	</VSlider>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { VSlider, VBtn } from "vuetify/components";
 
-export default defineComponent({});
+export default defineComponent({
+	components: {
+		VSlider,
+		VBtn
+	},
+	data: () => ({
+		value: 0
+	}),
+	methods: {
+		decrement() {
+			this.value--;
+		},
+		increment() {
+			this.value++;
+		}
+	}
+});
 </script>
