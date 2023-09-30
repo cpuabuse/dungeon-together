ff68b4c1560bd48f550935c552f207903167cd8a
Yano <51319482+yanocv@users.noreply.github.com>
2023-09-30 11:17:36 +0900

    Add: Animation for loading

--- src/app/vue/components/uuid-search.vue
+++ src/app/vue/components/uuid-search.vue
@@ -3,6 +3,7 @@
 -->
 
 <template>
+	<!-- Prevent not to trigger potential parent form -->
 	<VTextField
 		v-model="model"
 		label="Search templates"
@@ -10,20 +11,32 @@
 		hide-details
 		variant="solo-filled"
 		flat
-		@click:append-inner="onClick"
+		:loading="isLoading"
+		@click:append-inner="onSearch"
+		@keydown.enter.prevent="onSearch"
 	></VTextField>
 
 	<VCheckbox v-model="isNotFoundLogged" label="Log not found" hide-details></VCheckbox>
 </template>
 
 <script setup lang="ts">
-import { Ref, ref } from "vue";
+import { Ref, ref, unref } from "vue";
 import { VCheckbox, VTextField } from "vuetify/components";
 import { useStore } from "vuex";
 import { ClientEntity } from "../../client/entity";
 import { UniverseState, UniverseStore } from "../../client/gui";
 import { Uuid } from "../../common/uuid";
 import { LogLevel } from "../../core/error";
+
+/**
+ * Return type of set timeout function.
+ */
+type TimeoutHandle = ReturnType<typeof setTimeout>;
+
+/**
+ * Time to wait before animation clears.
+ */
+const loadingTimeout: number = 1000;
 
 /**
  * Model for text field.
@@ -36,6 +49,11 @@ const model: Ref<string> = ref("");
 const isNotFoundLogged: Ref<boolean> = ref(false);
 
 /**
+ * Loading timeout handle.
+ */
+let loadingTimeoutHandle: Ref<TimeoutHandle | null> = ref(null);
+
+/**
  * Universe store.
  */
 const {
@@ -43,13 +61,33 @@ const {
 }: UniverseStore = useStore<UniverseState>();
 
 /**
+ * Loading animation triggered temporarily to indicate to user that search is/was processed.
+ *
+ * @remarks
+ * Since search itself is almost instantaneous, the delay is to be used rather than completion of the search to remove animation.
+ */
+const isLoading: Ref<boolean> = ref(false);
+
+/**
  * On click callback.
  */
-function onClick(): void {
+function onSearch(): void {
 	/**
 	 * Local message array type.
 	 */
 	type Message = Parameters<(typeof universe)["log"]>[0];
+
+	// Enable animation
+	isLoading.value = true;
+
+	// Process of removal of animation
+	const loadingTimeoutHandleUnRef: TimeoutHandle | null = unref(loadingTimeoutHandle);
+	if (loadingTimeoutHandleUnRef !== null) {
+		clearTimeout(loadingTimeoutHandleUnRef);
+	}
+	loadingTimeoutHandle.value = setTimeout(() => {
+		isLoading.value = false;
+	}, loadingTimeout);
 
 	let uuid: Uuid = model.value;
 	let entity: ClientEntity = universe.getEntity({ entityUuid: uuid });
@@ -60,7 +98,7 @@ function onClick(): void {
 		messages.push({
 			data: entity,
 			level: LogLevel.Informational,
-			message: `Entity ${isEntityFound ? "" : "NOT "}found with UUID="${uuid}")`
+			message: `Entity ${isEntityFound ? "" : "NOT "}found with UUID="${uuid}"`
 		});
 	}
 
@@ -69,7 +107,7 @@ function onClick(): void {
 	} else {
 		universe.log({
 			level: LogLevel.Informational,
-			message: `Nothing found with UUID="${uuid}")`
+			message: `Nothing found with UUID="${uuid}"`
 		});
 	}
 }
