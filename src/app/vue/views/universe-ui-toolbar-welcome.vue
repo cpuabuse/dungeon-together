e85bf4a4198522c0f2d041d8729f4948a8d9e490
Yano <51319482+yanocv@users.noreply.github.com>
2023-12-26 14:33:46 +0900

    Add: Subtext to welcome screen

--- src/app/vue/views/universe-ui-toolbar-welcome.vue
+++ src/app/vue/views/universe-ui-toolbar-welcome.vue
@@ -7,28 +7,26 @@
 
 		<VDivider />
 
-		<div>
+		<div class="pt-4">
 			<strong>Keyboard controls</strong>
 			<VRow class="align-center" justify="space-around">
 				<!-- Movement section -->
 				<VCol cols="auto">
-					<VRow no-gutters>
-						<VCol v-for="(panel, panelKey) in keyboardControlPanels" :key="panelKey">
-							<VContainer>
-								<!-- First row of keys -->
-								<VRow no-gutters justify="center">
-									<VCol cols="auto">
-										<VKbd class="universe-ui-toolbar-welcome-keyboard">{{ panel[0] }}</VKbd>
-									</VCol>
-								</VRow>
+					<VRow no-gutters justify="space-around">
+						<VCol v-for="(panel, panelKey) in keyboardControlPanels" :key="panelKey" class="mx-2">
+							<!-- First row of keys -->
+							<VRow no-gutters justify="center">
+								<VCol cols="auto">
+									<VKbd class="universe-ui-toolbar-welcome-keyboard">{{ panel[0] }}</VKbd>
+								</VCol>
+							</VRow>
 
-								<!-- Second row of keys -->
-								<VRow class="flex-nowrap" no-gutters justify="center">
-									<VCol v-for="(button, buttonKey) in panel.slice(1)" :key="buttonKey" cols="auto">
-										<VKbd class="universe-ui-toolbar-welcome-keyboard">{{ button }}</VKbd>
-									</VCol>
-								</VRow>
-							</VContainer>
+							<!-- Second row of keys -->
+							<VRow class="flex-nowrap" no-gutters justify="center">
+								<VCol v-for="(button, buttonKey) in panel.slice(1)" :key="buttonKey" cols="auto">
+									<VKbd class="universe-ui-toolbar-welcome-keyboard">{{ button }}</VKbd>
+								</VCol>
+							</VRow>
 						</VCol>
 					</VRow>
 
@@ -39,10 +37,16 @@
 				<!-- Individual key section -->
 				<VCol v-for="(panel, panelKey) in keyboardIndividualsPanels" :key="panelKey" cols="auto">
 					<VRow v-for="(panelEntry, panelEntryKey) in panel" :key="panelEntryKey" no-gutters>
-						<VCol cols="auto" align-self="center"
-							><VKbd class="universe-ui-toolbar-welcome-keyboard">{{ panelEntry.key }}</VKbd></VCol
-						>
-						<VCol cols="auto" align-self="center">{{ panelEntry.description }}</VCol>
+						<VCol cols="auto" align-self="center">
+							<VKbd class="universe-ui-toolbar-welcome-keyboard">{{ panelEntry.keyboardKey }}</VKbd>
+						</VCol>
+						<VCol cols="auto" align-self="center">
+							{{ panelEntry.description }}
+							<template v-if="panelEntry.subtext">
+								<br />
+								<div class="text-subtitle-2">{{ panelEntry.subtext }}</div>
+							</template>
+						</VCol>
 					</VRow>
 				</VCol>
 			</VRow>
@@ -50,7 +54,8 @@
 
 		<VDivider />
 
-		<div>
+		<!-- Mouse control section -->
+		<div class="pt-4">
 			<strong>Mouse controls</strong>
 			<VRow>
 				<!-- Panel section -->
@@ -79,8 +84,12 @@
 		<div class="pt-4">
 			<strong>Entity</strong>
 			<ul>
-				<li class="d-flex align-center"><BaseIcon icon="fa-khanda" class="pr-1 pb-1 icon" /> Attack</li>
-				<li class="d-flex align-center"><BaseIcon icon="fa-hand" class="pr-1 pb-1 icon" />Use</li>
+				<li class="d-flex align-center">
+					<BaseIcon icon="fa-khanda" class="pr-1 pb-1 universe-ui-toolbar-welcome-ui-action" /> Attack
+				</li>
+				<li class="d-flex align-center">
+					<BaseIcon icon="fa-hand" class="pr-1 pb-1 universe-ui-toolbar-welcome-ui-action" />Use
+				</li>
 			</ul>
 		</div>
 	</div>
@@ -88,18 +97,45 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { VCol, VContainer, VDivider, VKbd, VRow } from "vuetify/components";
+import { VCol, VDivider, VKbd, VRow } from "vuetify/components";
 import { BaseIcon } from "../components";
 
 export default defineComponent({
-	components: { BaseIcon, VCol, VContainer, VDivider, VKbd, VRow },
+	components: { BaseIcon, VCol, VDivider, VKbd, VRow },
 
 	/**
 	 * Keyboard values.
 	 *
 	 * @returns Keyboard values
 	 */
-	data() {
+	data(): {
+		/**
+		 * Keyboard keys for movement.
+		 */
+		keyboardControlPanels: Array<[string, string, string, string]>;
+
+		/**
+		 * Section for sets of keys and descriptions.
+		 */
+		keyboardIndividualsPanels: Array<
+			Array<{
+				/**
+				 * Description of the keyboard key.
+				 */
+				description: string;
+
+				/**
+				 * Actual keyboard key.
+				 */
+				keyboardKey: string;
+
+				/**
+				 * Optional subtext for keyboard keys.
+				 */
+				subtext?: string;
+			}>
+		>;
+	} {
 		return {
 			keyboardControlPanels: [
 				["W", "A", "S", "D"],
@@ -107,10 +143,10 @@ export default defineComponent({
 			] as const,
 			keyboardIndividualsPanels: [
 				[
-					{ description: "Camera level up", key: "O" },
-					{ description: "Camera level down", key: "P" }
+					{ description: "Camera level up", keyboardKey: "O" },
+					{ description: "Camera level down", keyboardKey: "P" }
 				],
-				[{ description: "Use", key: "E", subtext: "(Use the ladder to descend)" }]
+				[{ description: "Use", keyboardKey: "E", subtext: "(Use the ladder to descend)" }]
 			]
 		};
 	}
@@ -132,5 +168,10 @@ li {
 .mouse-icon {
 	scale: 2;
 	margin: 2em;
+}
+
+.universe-ui-toolbar-welcome-ui-action {
+	scale: 1.5;
+	margin: 1em;
 }
 </style>
